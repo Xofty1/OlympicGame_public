c5d20051329bdbf026eb3a769a3a123d97479789
Artem <blekart04@mail.ru>
2023-11-28 01:40:18 +0300

    4 pages

---
+++ script.js
@@ -0,0 +1,91 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var checkbox = document.getElementById("menu-toggle");
+  var menu = document.getElementById("menu");
+  var clickCount = 0;
+
+  checkbox.addEventListener("change", function () {
+    clickCount++;
+
+    if (clickCount % 2 === 0) {
+      menu.classList.toggle("animate");
+    }
+  });
+});
+
+function revealText(element) {
+  var hiddenText = element.querySelector(".hidden-text");
+  if (hiddenText.style.display === "none" || hiddenText.style.display === "") {
+    hiddenText.style.display = "inline";
+  } else {
+    hiddenText.style.display = "none";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var videos = document.querySelectorAll(".my-video");
+
+  var observer = new IntersectionObserver(
+    function (entries) {
+      entries.forEach(function (entry) {
+        var video = entry.target;
+
+        if (entry.isIntersecting) {
+          video.play();
+        } else {
+          // Видео не видно, выключаем его
+          video.pause();
+        }
+      });
+    },
+    { threshold: 0.77 }
+  );
+
+  videos.forEach(function (video) {
+    observer.observe(video);
+  });
+
+  // Обработчик события изменения видимости документа
+  document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+      // Документ стал невидимым, поэтому выключаем все видео
+      videos.forEach(function (video) {
+        video.pause();
+      });
+    } else {
+      // Документ стал видимым, проверяем, если видео в зоне видимости, и включаем их
+      videos.forEach(function (video) {
+        var videoContainerRect = video.getBoundingClientRect();
+        if (
+          videoContainerRect.top < window.innerHeight &&
+          videoContainerRect.bottom >= 0
+        ) {
+          video.play();
+        }
+      });
+    }
+  });
+});
+
+function changeSize(element) {
+  var hiddenText = element.querySelector(".hidden-text-new");
+
+  // Используем атрибут данных для хранения состояния
+  var isIncreased = element.getAttribute("data-increased") === "true";
+
+  // Переключаемся между увеличением и уменьшением размеров
+  if (isIncreased) {
+    element.style.width = 12 + "vw";
+    element.style.height = 12 + "vw";
+    element.style.borderRadius = "50%";
+    hiddenText.style.display = "none"; // Скрываем текст при уменьшении
+  } else {
+    element.style.width = 22 + "vw";
+    element.style.height = "auto";
+    element.style.borderRadius = "0";
+    element.style.borderRadius = 7 + "px";
+    hiddenText.style.display = "block"; // Показываем текст при увеличении
+  }
+
+  // Инвертируем состояние и обновляем атрибут данных
+  element.setAttribute("data-increased", !isIncreased);
+}
